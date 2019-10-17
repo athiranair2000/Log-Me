eb6cce52116beb20955a5f6d19eb2d510f663c8a
Athi_Nair_2000 <athinair2000@gmail.com>
2019-10-17 22:46:55 +0530

    Update __init__.py

--- my-project/__init__.py
+++ my-project/__init__.py
@@ -1,18 +1,20 @@
-from flask import flask__
-from flask_sqlalchemy import flask_sqlalchemy
+from flask import Flask
+from flask_sqlalchemy import flask_SQLAlchemy
 from flask import LoginManager 
+from flask_migrate import Migrate
 db=SQLAlchemy()
 # initialising SQLALchemy to connect it with model
 
 def create_app():
 # Initialise the app
-app=Flask(__name__,instance_relative_config=True)
-app.config['SECRET_KEY']=''
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite://db.sqlite'
-db.init_app(app)
-login_manager=LoginManager()
-login_manager.login_view='auth.login'
-login_manager.init_app(app)
+	app=Flask(__name__,instance_relative_config=True)
+	db=SQLALchemy(app)
+	migrate = Migrate(app,db)
+	manager=Manager(app)
+	db.init_app(app)
+	login_manager=LoginManager()
+	login_manager.login_view='auth.login'
+	login_manager.init_app(app)
 
 # blue print for auth routes in application
 from .auth import auth as auth_blueprint
@@ -22,17 +24,24 @@ app.register_blueprint(auth_blueprint)
 from .main import main as main_blueprint
 app.register_blueprint(main_blueprint)
 
-return app
+
 
 
 # Load the views
 
-From app import views
+from app import views
 
-app.config.from_object('config')
+from app import routes,models
+
 
 from .models import User
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
+
+
+
+
+if __name__=='__main__':
+	manager.run()

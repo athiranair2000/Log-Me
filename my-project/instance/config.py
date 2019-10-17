261c517d9528c094fe16118a57ee4750e2500376
Athi_Nair_2000 <athinair2000@gmail.com>
2019-10-17 20:33:22 +0530

    Update config.py

--- my-project/instance/config.py
+++ my-project/instance/config.py
@@ -1,1 +1,10 @@
-DEBUG=True
+import os
+
+basedir=os.path.abspath(os.path.dirname(__file__)) # BASE_DIR is pointing to the parent directory of PROJECT_ROOT.
+                                                    # simply removes the last segment of a path.
+                                                    # storing the full path to the directory the module lives in in PROJECT_ROOT.
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \ 'sqlite:///'+os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATION=False
+    
